2c5b2db16ea84febaf634c9438aebccc2702164e
srihithamandalapu9 <78907854+srihithamandalapu9@users.noreply.github.com>
2021-02-20 19:49:15 +0530

    Add files via upload
    HTML5&CSS3 Assignment

---
+++ HTML5&CSS3-Morning/profile_script.js
@@ -0,0 +1,8 @@
+function linkedin_page(){
+    console.log("clicked")
+    window.open("https://www.linkedin.com/in/srihitha-mandalapu-00b680188/","_blank")
+}
+
+function github_page(){
+    window.open("https://github.com/srihithamandalapu9","_blank")
+}
